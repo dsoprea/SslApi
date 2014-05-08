ef532c443c571e969c093326ed7e1bfa528498eb
Dustin Oprea <myselfasunder@gmail.com>
2014-05-07 23:53:53 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
 from setuptools import setup, find_packages
-import sys, os
-
-version = '0.2.0'
 
 setup(name='ssl_api',
-      version=version,
+      version='0.2.0',
       description="A certificate-authority API.",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description="",
+      classifiers=[],
       keywords='ssl openssl ca certificate authority api',
       author='Dustin Oprea',
       author_email='myselfasunder@gmail.com',
       url='',
       license='GPL 2',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=find_packages(exclude=['tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # -*- Extra requirements: -*-
+          'M2Crypto'
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+      scripts=[
+            'create_ca_identity',
+            'create_subordinate',
+      ]
+)

a803a24568a247019d29b1e20b69d5a7b526735a
Dustin Oprea <myselfasunder@gmail.com>
2014-05-27 14:23:27 -0400

    Version bump. Should've been in last tag.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ssl_api',
-      version='0.2.29',
+      version='0.2.30',
       description="A certificate-authority API.",
       long_description="",
       classifiers=[],

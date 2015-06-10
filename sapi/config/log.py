b066e9eb5bbe650b953be4130043115d1f8b9413
Dustin Oprea <myselfasunder@gmail.com>
2015-06-09 21:26:59 -0400

    Updated log config so stuff goes into syslog.

--- sapi/config/log.py
+++ sapi/config/log.py
@@ -1,17 +1,40 @@
+import os
 import logging
+import logging.handlers
 
-import sapi.config.general
+_LOGGER = logging.getLogger()
 
-logger = logging.getLogger()
+_IS_DEBUG = bool(int(os.environ.get('DEBUG', '0')))
+_DO_DEBUG_SCREEN_LOG = bool(int(os.environ.get('DO_DEBUG_SCREEN_LOG', '0')))
+_SYSLOG_FACILITY = logging.handlers.SysLogHandler.LOG_LOCAL0
 
-def _configure_console_log():
-    ch = logging.StreamHandler()
+def _configure_logs():
+    logger = logging.getLogger()
 
-    FMT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    formatter = logging.Formatter(FMT)
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    if _IS_DEBUG is True or _DO_DEBUG_SCREEN_LOG is True:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
 
-if sapi.config.general.IS_DEBUG is True:
-    _configure_console_log()
-    logger.setLevel(logging.DEBUG)
+    # Configure screen.
+    if _IS_DEBUG is True or _DO_DEBUG_SCREEN_LOG is True:
+        format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        formatter = logging.Formatter(format)
+
+        sh = logging.StreamHandler()
+        sh.setFormatter(formatter)
+        logger.addHandler(sh)
+
+    # Configure Syslog.
+    if os.path.exists('/dev/log') is True:
+        format = '%(name)s - %(levelname)s - %(message)s'
+        formatter = logging.Formatter(format)
+
+        sh2 = logging.handlers.SysLogHandler(
+                address='/dev/log',
+                facility=_SYSLOG_FACILITY)
+
+        sh2.setFormatter(formatter)
+        logger.addHandler(sh2)
+
+_configure_logs()
